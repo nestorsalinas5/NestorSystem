f16283b1db37797976e70aaf2b49ccc5146365a5
nestorsalinas5 <105450797+nestorsalinas5@users.noreply.github.com>
2025-09-06 16:37:25 -0300

    Update Icons.tsx

--- components/Icons.tsx
+++ components/Icons.tsx
@@ -59,6 +59,17 @@ export const BudgetIcon = () => (
     </svg>
 );
 
+export const InquiryIcon = () => (
+    <svg {...iconProps} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+        <path d="M16 4h2a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h2"></path>
+        <rect x="8" y="2" width="8" height="4" rx="1" ry="1"></rect>
+        <path d="M12 11h4"></path>
+        <path d="M12 16h4"></path>
+        <path d="M8 11h.01"></path>
+        <path d="M8 16h.01"></path>
+    </svg>
+);
+
 export const SettingsIcon = () => (
   <svg {...iconProps} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
     <circle cx="12" cy="12" r="3" />
