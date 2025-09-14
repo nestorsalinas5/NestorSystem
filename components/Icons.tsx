43e85dc7afdafe2578bb2913f699ae4733a7ef5c
nestorsalinas5 <105450797+nestorsalinas5@users.noreply.github.com>
2025-09-14 01:59:13 -0300

    Update Icons.tsx

--- components/Icons.tsx
+++ components/Icons.tsx
@@ -261,3 +261,9 @@ export const LogoIconOnly = ({ className = 'w-10 h-10' }: { className?: string }
         </g>
     </svg>
 );
+
+export const MessageSquareIcon = () => (
+    <svg {...iconProps} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+        <path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"></path>
+    </svg>
+);
