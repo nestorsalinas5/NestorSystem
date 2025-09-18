61635fe66744e616d9a926c6399fde910457adb3
nestorsalinas5 <105450797+nestorsalinas5@users.noreply.github.com>
2025-09-17 22:34:44 -0300

    Update Icons.tsx

--- components/Icons.tsx
+++ components/Icons.tsx
@@ -1,4 +1,5 @@
 
+
 import React from 'react';
 
 const iconProps = {
@@ -265,5 +266,14 @@ export const LogoIconOnly = ({ className = 'w-10 h-10' }: { className?: string }
 export const MessageSquareIcon = () => (
     <svg {...iconProps} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
         <path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"></path>
+    </svg>
+);
+
+export const ClipboardListIcon = () => (
+    <svg {...iconProps} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+        <path d="M16 4h2a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h2"></path>
+        <rect x="8" y="2" width="8" height="4" rx="1" ry="1"></rect>
+        <line x1="9" y1="12" x2="15" y2="12"></line>
+        <line x1="9" y1="16" x2="15" y2="16"></line>
     </svg>
 );
